484f1d8821fe47f586801065e992dc5634e68243
Priyanshu Kumawat <priyanshukumawat434@gmail.com>
2023-12-23 18:10:58 +0530

    Card Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import './App.css'
+// import './App.css'
+import Card from './components/Card'
 
 function App() {
 
+  const cardDetails = { id: "CAM-10", title: "Conduct Security Vulnerability Assessment", tag: ["Feature Request"], userId: "usr-4", status: "Backlog", priority: 1 }
+
   return (
     <>
+      <Card {...cardDetails} />
     </>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,5 @@
+import "../styles/Module.Card.css"
+
 interface Card {
     id: string;
     title: string;
@@ -7,14 +9,34 @@ interface Card {
     priority: number;
 }
 
+const users = [
+    { id: "usr-1", name: "Anoop sharma", available: false }, { id: "usr-2", name: "Yogesh", available: true }, { id: "usr-3", name: "Shankar Kumar", available: true }, { id: "usr-4", name: "Ramesh", available: true }, { id: "usr-5", name: "Suresh", available: true }]
+
 const Card = ({ id, title, tag, userId, priority }: Card) => {
+
+    const priorityClass = `card-priority-${priority}`;
+    const user = users.find(user => user.id === userId);
+    const userName = user?.name.split(" ")
+    const userNameIcon = userName.map(name => name[0].toUpperCase()).join('')
+
     return (
-        <div>
-            <p>{id}</p>
-            <p>{userId}</p>
-            <p>{title}</p>
-            <p>{priority}</p>
-            <p>{tag[0]}</p>
+        <div className="card">
+
+            <div className="card-header">
+                <p className="card-id">{id}</p>
+                <p className="card-userId">{userNameIcon}</p>
+            </div>
+
+            <p className="card-title">{title}</p>
+
+            <div className="card-footer">
+                <p className={`${priorityClass} card-priority`}>{priority}</p>
+                <p className="card-tag">
+                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" className="icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{ color: "gray" }}><path d="M232,128A104,104,0,1,1,128,24,104.13,104.13,0,0,1,232,128Z"></path></svg>
+                    &nbsp;
+                    {tag[0]}</p>
+            </div>
+
         </div>
     );
 }

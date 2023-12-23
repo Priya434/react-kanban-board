ca6c1ebf87530967ed017d48ddd40da34e4e8481
Priyanshu Kumawat <priyanshukumawat434@gmail.com>
2023-12-23 19:08:46 +0530

    Integrate API and change naming from card to ticket

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,68 @@
 // import './App.css'
-import Card from './components/Card'
+import Ticket from './components/Ticket.tsx'
+import { useEffect, useState } from 'react';
+
+export interface TicketDetailsType {
+  id: string;
+  title: string;
+  tag: string[];
+  userId: string;
+  status: string;
+  priority: number;
+}
+
+export interface UserDetailsType {
+  id: string;
+  name: string;
+  available: boolean;
+}
+
+export interface ApiDataType {
+  tickets: TicketDetailsType[];
+  users: UserDetailsType[];
+}
 
 function App() {
 
-  const cardDetails = { id: "CAM-10", title: "Conduct Security Vulnerability Assessment", tag: ["Feature Request"], userId: "usr-4", status: "Backlog", priority: 1 }
+  const apiUrl = "https://tfyincvdrafxe7ut2ziwuhe5cm0xvsdu.lambda-url.ap-south-1.on.aws/ticketAndUsers";
+
+  const [apiData, setapiData] = useState<ApiDataType>();
+
+  async function fetchApiData() {
+    // fetch(apiUrl)
+    //   .then(response => response.json())
+    //   .then(data => setapiData(data))
+    //   .catch(error => console.error('Error:', error));
+
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Process data from the first API
+        // console.log('Data from API:', data);
+        setapiData(data)
+
+        // Fetch data from the second API endpoint
+        // return fetch(apiUrl2);
+      })
+  }
+
+  useEffect(() => {
+    fetchApiData();
+  }, [])
 
   return (
     <>
-      <Card {...cardDetails} />
+      {console.log(apiData)}
+      <div>
+        {apiData ? apiData.tickets.map((data) => (
+          <Ticket key={data.id} {...data} />
+        )) : null}
+      </div>
     </>
   )
 }
--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -1,23 +1,15 @@
-import "../styles/Module.Card.css"
-
-interface Card {
-    id: string;
-    title: string;
-    tag: string[];
-    userId: string;
-    status: string;
-    priority: number;
-}
+import "../styles/Module.Ticket.css"
+import { TicketDetailsType } from "../App.tsx";
 
 const users = [
     { id: "usr-1", name: "Anoop sharma", available: false }, { id: "usr-2", name: "Yogesh", available: true }, { id: "usr-3", name: "Shankar Kumar", available: true }, { id: "usr-4", name: "Ramesh", available: true }, { id: "usr-5", name: "Suresh", available: true }]
 
-const Card = ({ id, title, tag, userId, priority }: Card) => {
+const Ticket = ({ id, title, tag, userId, priority }: TicketDetailsType) => {
 
     const priorityClass = `card-priority-${priority}`;
     const user = users.find(user => user.id === userId);
     const userName = user?.name.split(" ")
-    const userNameIcon = userName.map(name => name[0].toUpperCase()).join('')
+    const userNameIcon = userName?.map(name => name[0].toUpperCase()).join('')
 
     return (
         <div className="card">
@@ -32,7 +24,7 @@ const Card = ({ id, title, tag, userId, priority }: Card) => {
             <div className="card-footer">
                 <p className={`${priorityClass} card-priority`}>{priority}</p>
                 <p className="card-tag">
-                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" className="icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{ color: "gray" }}><path d="M232,128A104,104,0,1,1,128,24,104.13,104.13,0,0,1,232,128Z"></path></svg>
+                    <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 256 256" className="icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{ color: "gray" }}><path d="M232,128A104,104,0,1,1,128,24,104.13,104.13,0,0,1,232,128Z"></path></svg>
                     &nbsp;
                     {tag[0]}</p>
             </div>
@@ -41,4 +33,4 @@ const Card = ({ id, title, tag, userId, priority }: Card) => {
     );
 }
 
-export default Card;
+export default Ticket;
